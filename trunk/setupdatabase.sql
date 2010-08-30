b38bc9a309a015a0d2f51036fefe4e28de9b8b71
jmh9072 <jmh9072@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2010-08-30 03:49:11 +0000

    New user accounts in the database should start at UID 2, not 271

--- trunk/setupdatabase.sql
+++ trunk/setupdatabase.sql
@@ -92,7 +92,7 @@ CREATE TABLE `user` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`),
   FULLTEXT KEY `pass` (`pass`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=271 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `user`
