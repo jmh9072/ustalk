95a587a90212ae6294af2e1aff48c639fdc5cfd9
firebird347 <firebird347@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2010-12-13 22:49:27 +0000

    Fix for Issue 13: Invalid JSON

--- uStalk/template/stalk_json.php
+++ uStalk/template/stalk_json.php
@@ -1,8 +1,8 @@
 {
-  'online':<?=time() - strtotime($data['on']) - 60 * 60 * 0 < 15 * 60 ? "true" : "false" ?>,
-  'uid':<?=json_encode($data['bid'])?>,
-  'avatar':<?=json_encode(ereg_replace("&", "&amp;", $data['avatar'])) ?>,
-  'name':<?=json_encode($data['name']) ?>,
-  'date':<?=json_encode(date("n/j/Y",strtotime($data['on']))) ?>,
-  'time':<?=json_encode(date("H:i",strtotime($data['on']))) ?>
+  "online":<?=time() - strtotime($data['on']) - 60 * 60 * 0 < 15 * 60 ? "true" : "false" ?>,
+  "uid":<?=json_encode($data['bid'])?>,
+  "avatar":<?=json_encode(ereg_replace("&", "&amp;", $data['avatar'])) ?>,
+  "name":<?=json_encode($data['name']) ?>,
+  "date":<?=json_encode(date("n/j/Y",strtotime($data['on']))) ?>,
+  "time":<?=json_encode(date("H:i",strtotime($data['on']))) ?>
 }
