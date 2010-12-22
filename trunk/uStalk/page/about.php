784bfb1483021e35691b4b2dffb42f192f516938
jmh9072 <jmh9072@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2010-12-22 03:59:04 +0000

    Added AJAX interface, default for browser-based pageloads; added licensing info from project homepage to repository

--- trunk/uStalk/page/about.php
+++ trunk/uStalk/page/about.php
@@ -6,3 +6,5 @@ if($HEAD)
 <h1>What is this? Who made it? How does it work? Can I help?</h1>
 <p>I'll answer these eventually, as soon as I get back from the bathroom...</p>
 -Man in Gorilla Suit and Cowboy hat
+<br /><br />
+<p>This website uses an AJAX loading bar from <a href="http://ajaxload.info">ajaxload.info</a>.</p>
