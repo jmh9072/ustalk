fe88808818e2d3cb7a8530687f84ed1f80825357
firebird347 <firebird347@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2010-12-23 05:41:39 +0000

    Misc. html fixes.

--- trunk/uStalk/page/news.php
+++ trunk/uStalk/page/news.php
@@ -11,6 +11,5 @@ if (array_key_exists('login_fail', $_GET) && $_GET["login_fail"])
 	echo "<br /><br /><font color='red'>Error: Username/Password Combination does not exist.</font>";
 }
 ?>
-</font>
 <h1>uStalk is back!</h1>
 <p>uStalk has been optimized in order to make sure that we do not lose access to bungie.net like we did before. Users' last post times are less frequently updated and we have reduced the number of page fetches we need to make per update.</p>
