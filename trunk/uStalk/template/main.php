0588ca16da776a23f150e7afc92a38657bdcb67f
firebird347 <firebird347@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2010-12-23 05:11:22 +0000

    Made it so you could add simple tracking code.

--- trunk/uStalk/template/main.php
+++ trunk/uStalk/template/main.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html
-PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
 <?
@@ -8,6 +6,10 @@ echo $data['head'];
 linkCSS('default');
 if (stristr($_SERVER['HTTP_USER_AGENT'], "AppleWebKit"))
 	linkCSS('webkit');
+
+if (file_exists('local_tracking.php')) {
+  include 'local_tracking.php';
+}
 ?>
 </head>
 <body>
