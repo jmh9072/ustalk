a7870e3500970276a7e4dfa7c50ecdb48860d237
Firebird347 <Firebird347@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2011-03-22 18:46:43 +0000

    Switch to XHTML lower case from upper case in HTML tags.

--- trunk/uStalk/template/ministalkee.php
+++ trunk/uStalk/template/ministalkee.php
@@ -3,9 +3,9 @@ $on = false;
 if(time() - strtotime($data['on']) < 60 * 15)
 	$on = true;
 ?>
-<TR<?=($on? ' style=\'background:url(http://i208.photobucket.com/albums/bb114/CavAvx/online.png) repeat;\'': '')?>>
-<TD><A HREF="http://www.bungie.net/Account/Profile.aspx?uid=<?=$data['bid']?>"> <img src="<?=$data['avatar']?>" ALT="Offline... Possibly."></A></TD>
-<TD><?=$data['name']?></TD>
-<TD><?=date("n/j/Y",strtotime($data['on']))?></TD>
-<TD><?=date("H:i",strtotime($data['on']))?></TD>
-</TR>
+<tr<?=($on? ' style=\'background:url(http://i208.photobucket.com/albums/bb114/CavAvx/online.png) repeat;\'': '')?>>
+<td><a href="http://www.bungie.net/Account/Profile.aspx?uid=<?=$data['bid']?>"> <img src="<?=$data['avatar']?>" alt="Offline... Possibly."></a></td>
+<td><?=$data['name']?></td>
+<td><?=date("n/j/Y",strtotime($data['on']))?></td>
+<td><?=date("H:i",strtotime($data['on']))?></td>
+</tr>
