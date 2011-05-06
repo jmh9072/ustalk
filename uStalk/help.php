cede02f8d335e14461df4e5348d9ce869e470214
jmh9072 <jmh9072@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2011-05-06 15:18:19 +0000

    Rest of the commit from r58

---
+++ uStalk/help.php
@@ -0,0 +1,8 @@
+<?
+require_once "common.php";
+
+global $error;
+$error = false;
+$page['name'] = 'help';
+include "index.php";
+?>
---
+++ uStalk/page/help.php
@@ -0,0 +1,11 @@
+<?
+if($HEAD)
+	return;
+?>
+<h1>Help Center</h1>
+<h4>This page houses an in-progress FAQ. If you have any questions you'd like answered, please PM them to <a href='http://www.bungie.net/account/profile.aspx?uid=1545001&page=PostMsg'>jmh9072</a> on bungie.net.</h4>
+
+<h2>How do I enter a user to track?</h2>
+<p>First, you need to find the UID (NOT memberID) for the user you want to track. These will always be in the URL of the user's gamertag (not bungie.net username). Look for the part of the URL that says uid=#########. If the user does not have a linked gamertag, you can find their UID by searching for them with the people finder. When you click on their bungie.net name there, the URL will contain the UID as described before.</p>
+<p>To enter their name, simply type or copy/paste it. If you have a nickname you can enter that instead. This field is not checked for consistency with the actual profile name.</p>
+<p>To enter their avatar, simply copy the URL for it into the text field. With most browsers, you should be able to find this URL by right clicking on the user's avatar and clicking "properties." Look for the URL on the window that opens. Note that this field is also not checked for consistency with the actual avatar, so if you wish, you can use an alternate image. This could be their Coup avatar, their Halo 3 player avatar, or anything else (note that any image sizes other than 90x90 may look odd on the page, though).
