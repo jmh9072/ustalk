58b8fb9ec2c6feb9fe95e1a663885b0fd09007e6
jmh9072 <jmh9072@e4a6e238-d8b2-31b3-58c7-b516536e84fd>
2012-02-23 20:01:55 +0000

    Added a little MySQL to the beanstalk processor

--- uStalk/beanstalk_processor.py
+++ uStalk/beanstalk_processor.py
@@ -1,6 +1,7 @@
 import beanstalkc
-from urllib.request import urlopen
+import urllib2
 import re
+import MySQLdb
 
 #make beanstalkd connection
 beanstalk = beanstalkc.Connection(host='localhost', port=11300)
@@ -11,11 +12,18 @@ job = beanstalk.reserve()
 #job.body will contain the UID of the user that needs updated
 
 #open database connection
+database = MySQLdb.connect (host = "localhost, user = "ustalk", passwd="password", db="ustalk")
 
-#retrieve our user's postID
+#retrieve our user's data
+cursor = database.cursor()
+cursor.execute("SELECT * FROM bungie WHERE id="+job.body) #do we need to escape job.body?
+row = cursor.fetchone()
+
 
 #get bungie forum page
 bungiethread = urlopen('http://www.bungie.net/Forums/posts.aspx?postID=' + postID)
+
+#make sure the last time we refreshed this user's data was more than 30 mintues ago
 
 #use regex to get last post time
 posthead = re.search(b"jmh9072<\/a>.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*last post: (.*) P", bungiethread.read())
@@ -23,7 +31,10 @@ print(posthead.group(1))
 
 #update info in database
 
-#remove job from queue and close connections
-##disconnect database
+#close cursor and disconnect database
+cursor.close()
+database.close()
+
+#remove job from queue and close connection
 job.delete()
 beanstalk.close()
